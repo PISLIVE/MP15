171e0dd6d99ffa1ac179753318f1a2c68a99e017
PISLIVE <boroprince9@gmail.com>
2026-03-31 17:14:57 +0530

    Fix Vercel deployment: SPA rewrites + env vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./app/pages/Login";
 import { Dashboard } from "./app/pages/Dashboard";
+import { History } from "./app/pages/History";
 import { Settings } from "./app/pages/Settings";
 import ProtectedRoute from "./app/components/ProtectedRoute";
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <ProtectedRoute>
-            <Settings />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <History />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Toaster richColors position="top-right" />
+    </>
   );
 }
 
-export default App;
+export default App;
